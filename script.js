f3c51867324a7c7ecf44c91a0555e3b9074afdd5
shm0609 <121483337+shm0609@users.noreply.github.com>
2022-12-27 19:30:23 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function inform(){
     alert("나는 alert 입니다.");
 }
+
+function grade(){
+    var kor = document.getElementById("kor1");
+    var kor = document.getElementById("kor2");
+    document.getElementById("first_g").innerHTML="185";
+    document.getElementById("second_g").innerHTML="185";
+    document.getElementById("third_g").innerHTML="185"
+}
